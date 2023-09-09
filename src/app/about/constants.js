4260a5f5e275d3bef54e6b3cbdb909bb433fe787
Kolawole Joshua <Kolawole.joshua@wemabank.com>
2023-09-09 11:28:20 +0100

    updates

--- src/app/about/constants.js
+++ src/app/about/constants.js
@@ -7,4 +7,8 @@ export const services = [
     name: "UI Design",
     desc: "A well-crafted user interface is the key to exceptional user experiences. Our UI design services are dedicated to enhancing the usability and visual appeal of your digital products.",
   },
+  {
+    name: "Web3 Implementation",
+    desc: "Web3 implementation services specialize in integrating smart contracts and blockchain capabilities into your web applications. Whether it's for decentralized finance (DeFi), non-fungible tokens (NFTs), or custom blockchain solutions, we've got the expertise to make it happen. Elevate your web app with the power of blockchain technology.",
+  },
 ];
